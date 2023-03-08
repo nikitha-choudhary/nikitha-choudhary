19256e264ee3c40f13d83e044d7e8ab29a23ff2e
nikitha-choudhary <126843571+nikitha-choudhary@users.noreply.github.com>
2023-03-08 09:34:32 +0530

    output

--- bubblesort.c
+++ bubblesort.c
@@ -29,7 +29,18 @@ void main()
 }
     
 /*
-Enter the size of array=23
+Enter the size of array=223
 Enter the elements of array----
- Enter the 1 element
+ Enter the 1 element12
+ Enter the 2 element34
+ Enter the 3 element45
+ Enter the 4 element78
+ Enter the 5 element87
+ Enter the 6 element89
+ Enter the 7 element98
+ Enter the 8 element65
+ Enter the 9 element90
+ Enter the 10 element78
+ Enter the 11 element64
+ Enter the 12 element
 */
