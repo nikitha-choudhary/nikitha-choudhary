7098df6399dc35a0084507e54c2fb523749353eb
nikitha-choudhary <126843571+nikitha-choudhary@users.noreply.github.com>
2023-03-08 09:17:41 +0530

    bubble sort

---
+++ bubblesort.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+void main()
+{
+    int arr[20],size,i, j, temp;
+    printf("Enter the size of array=");
+    scanf("%d",& size);
+    printf("Enter the elements of array----");
+    for(i=0;i<size;i++)
+    {
+        printf("\n Enter the %d element",i+1);
+        scanf("%d",& arr[i]);
+    }
+    
+    for(i=0;j<size n-1;j++)
+    for(j=0;j<size n-1;j++)
+    {
+        if(arr[j]>arr[j+1])
+        {
+            temp=arr[j];
+            arr[j]=arr[j+1];
+            arr[j+1]=temp;
+        }
+    }
+    
+    
+        printf("\n Sorted array is----");
+        for(i=0;i<size;i++)
+        printf("\t %d",arr[i]);
+}
+    
